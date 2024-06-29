8f5f68c3946588ec7af685a0dfadfccf742c840b
Deren Vural <derenv@live.co.uk>
2024-06-29 14:39:00 +0100

    Modified reset.sh: - Updated printf statements with more info - skipped using variables for RUNCHECK - added some else statements to checks - added links to mozilla docs for HTTP response codes - switched to using systemd instead of podman for restart
    Signed-off-by: Deren Vural <derenv@live.co.uk>

--- reset.sh
+++ reset.sh
@@ -46,24 +46,28 @@ if [ -f "${SCRIPT_DIR:?}/config.conf" ]; then
     printf "variable DOCKER_IP_RANGE not set!\n"; exit 1
   fi
 
-  printf "config working..\n"
+  printf "config file present and defines required variables..\n"
 else
-  printf "missing config file!\n"; exit 1
+  printf "config file missing!\n"; exit 1
 fi
 ##
 
 ## Check container running ##
-RUNCHECK=$(podman ps | grep nginx)
-if [[ "${RUNCHECK}" == "" ]]; then
+if [[ "$(podman ps | grep nginx)" == "" ]]; then
   printf "NGinx container not running..\n"; exit 1
+else
+  printf "NGinx container running..\n"
 fi
 
 ## Check AIO http response ##
-printf "Checking URL response..\n"
+printf "Checking URL HTTP response..\n"
 RESPONSE=$(curl -s -o /dev/null -I -w "%{http_code}" "https://${WEBSITE_URL:?}/")
 
+# Respond to status, depending on HTTP response
+#https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/504
+#https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/502
 if [[ "${RESPONSE:?}" == "502" ]] || [[ "${RESPONSE:?}" == "504" ]]; then
-  printf "Response indicates new IP required!\n"
+  printf "HTTP Response \'%s\' indicates new IP required!\n" "${RESPONSE:?}"
 
   ## Get new IP address from docker ##
   # Reset DOCKER_HOST value to empty, rootfull docker is then used
@@ -77,18 +81,19 @@ if [[ "${RESPONSE:?}" == "502" ]] || [[ "${RESPONSE:?}" == "504" ]]; then
   CONTAINER_COUNT=$(printf "%s" "${CONTAINERS:?}" | jq 'length')
   if [[ "${CONTAINER_COUNT:?}" -eq 0 ]]; then
     printf "No containers present in Docker..\n"; exit 1
+  else
+    printf "Containers present: \'%s\'\n" "${CONTAINER_COUNT:?}"
   fi
-  printf "Containers present: \'%s\'\n" "${CONTAINER_COUNT:?}"
 
   # Check the AIO's Apache container exists
   APACHE_CONTAINER=$(printf "%s" "${CONTAINERS:?}" | grep -e "\"Name\": \"nextcloud-aio-apache\"" -A3)
   if [[ "$?" -ne 0 ]]; then
     printf "No nextcloud AIO apache container present..\n"; exit 1
+  else
+    printf "Apache container present!\n"
   fi
-  printf "Apache container present!\n"
 
   # Store "IPv4Address" value in variable
-  OLD_IFS="$IFS"
   IFS="\"" read -a ip_array <<< $(printf "%s" "${APACHE_CONTAINER:?}" | grep -e "IPv4Address")
   NEW_IP="${ip_array[3]::-3}"
 
@@ -109,22 +114,15 @@ if [[ "${RESPONSE:?}" == "502" ]] || [[ "${RESPONSE:?}" == "504" ]]; then
 
 
   ## Restart NGINX container ##
-  # Re-set DOCKER_HOST
-  DOCKER_HOST="unix:///run/user/1000/podman/podman.sock"
-
-  # Throws this error: `Error: crun: executable file `/init` not found in $PATH: No such file or directory: OCI runtime attempted to invoke a command that was not found`, can ignore..
-  #podman restart nginx && printf "Container restarted..\n"
-  # switched to the below.. needs confirmation that it works
-
-  # Stop and delete podman container
-  printf "Stopping NGinx container..\n"
-  podman stop nginx  && printf "NGinx container stopped..\n"
-
-  # Start NGinx service
-  systemctl start container-nginx.service && printf "Service \'container-nginx.service\' started..\n"
+  #### USE SYSTEMD
+  printf "Restarting NGinx container using systemd service..\n"
+  systemctl --user restart nginx-container.service
 
   # Wait 10 seconds to give podman time to restart the container..
   sleep 10s
+
+  # Re-set DOCKER_HOST to use podman
+  DOCKER_HOST="unix:///run/user/1000/podman/podman.sock"
 
   # Check nginx container status using health check
   # (NOTE: Podman puts a carriage return ('\r') at the end for some reason, windows style? hence trim command)
@@ -135,12 +133,12 @@ if [[ "${RESPONSE:?}" == "502" ]] || [[ "${RESPONSE:?}" == "504" ]]; then
     printf "Something's gone very wrong, container health check failed: \'%s\'\n" "${CONTAINER_STATUS:?}"; exit 1
   fi
   ##
+#https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200
+#https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/302
 elif [[ "${RESPONSE:?}" == "200" ]] || [[ "${RESPONSE:?}" == "302" ]]; then
-  printf "No update required..\n"
-#elif [[ "${RESPONSE:?}" == "504" ]]; then
-#  printf "Somoething\'s gone very wrong, http response is \'%s\' i.e. a Gateway Timeout: \'https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/504\'..\n" "${RESPONSE:?}"
+  printf "HTTP Response \'%s\' indicates no update required..\n" "${RESPONSE:?}"
 else
-  printf "Something\'s gone very wrong, unknown http response code: \'%s\'\n" "${RESPONSE:?}"; exit 1
+  printf "Something\'s gone very wrong, unhandled HTTP response code: \'%s\'\n" "${RESPONSE:?}"; exit 1
 fi
 
 exit 0
